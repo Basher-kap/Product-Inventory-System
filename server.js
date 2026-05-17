236ffa7d54e469f99a1eeac5c409a2c8bbbe1e39
Basher-kap <kap.bash@gmail.com>
2026-05-17 22:44:24 +0800

    refactor: reorganize middleware setup in server.js

--- server.js
+++ server.js
@@ -1,3 +1,10 @@
+const express = require('express');
+const app = express();
+
+// middleware
+app.use(express.json());
+app.use(express.static(__dirname));
+
 // routes
 app.use('/api', require('./app/routes/auth'));
 app.use('/api', require('./app/routes/profile'));
